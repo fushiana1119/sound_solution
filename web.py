d9f66f9195a5cba13ddab569b2326e4b8397a3e7
fushiana <vip_ktkr@hotmail.com>
2017-10-07 12:19:51 +0900

    Create web.py

---
+++ web.py
@@ -0,0 +1,40 @@
+# coding:utf-8
+import requests
+import json
+
+from datetime import datetime
+
+def uploadSensorValues(freq,level):
+    url = 'http://192.168.10.101:8888/sound_solution/sensvalues.php'
+
+    sensorsdata = {'datetime':datetime.now().strftime("%Y/%m/%r %H:%M:%S"),'freq':freq,'level':level}
+    print json.dumps(sensorsdata)
+
+    headers = {'content-type': 'application/json'}
+
+    res = requests.post(url, data=json.dumps(sensorsdata), headers=headers, verify=False)
+
+    print res.json()
+    pass
+
+def main():
+
+
+#   変数宣言
+    freq = 0.0    #周波数
+    level = 0.0   #レベル
+    date = 0
+    print(date)
+    for cnt in open('sample.txt').readlines():
+#       末尾のデータを格納
+        data = cnt[:-1].split(' ')
+        freq = data[0]
+        level = data[1]
+#       データベースの主キーにするための現在時刻の取得
+
+#       データベースにアクセスし、指定のデータを書き込む
+        uploadSensorValues(freq,level)
+
+
+if __name__ == '__main__':
+    main()
